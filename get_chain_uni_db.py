9fd233bd5167667f30f6431b93ffa063be45fcde
RubbishRecognition <aleksandr.alekseev@frtk.ru>
2016-09-05 12:52:44 +0300

    new sql db and scripts

---
+++ get_chain_uni_db.py
@@ -0,0 +1,24 @@
+import sqlite3
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('gene')
+args = parser.parse_args()
+
+conn = sqlite3.connect('pdb_chain_uniprot_paired.db')
+c = conn.cursor()
+
+output = open('../tmp/chain_uni_out.txt', 'w')
+
+for row in c.execute('SELECT item_id_b FROM protein_actions WHERE item_id_a=?', (args.gene,)):
+    #print row[0][1]
+    #out_list = list(row[0])
+    #for item in row[0]:
+    output.write("%s\n" % row[0])
+
+for row in c.execute('SELECT item_id_a FROM protein_actions WHERE item_id_b=?', (args.gene,)):
+    #print row[0][1]
+    #output.write(row[0])
+    #out_list = list(row[0])
+    #for item in row[0]:
+    output.write("%s\n" % row[0])
